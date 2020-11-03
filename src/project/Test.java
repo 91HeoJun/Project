79af701fe08634fd215b7ddf851607dba54292b7
sooyeun <blind1215@gmail.com>
2020-11-03 17:46:56 +0900

    멘트 이름 수정

--- src/project/Test.java
+++ src/project/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("눈치게임 1");
+		System.out.println("1등"); //도수연 
 
 		// 작성하면 이름 주석으로 달아보면 보기 편할 것 같아요
 
@@ -14,17 +14,10 @@ public class Test {
 
 		System.out.println("수성씨, 영우씨 추가 확인했습니다"); // 허준
 
-		System.out.println("올립니다"); // 황진구
-
-
 		System.out.println("꿀팁 뿌립니다. !"); // 주영우
 
 		System.out.println("이마트 와인코너 가셔서 모스카토 2병에 18000원에 살수있습니다. 원래는 한병에 15000원 이상이에요"); // 주영우
-
-
-
 		System.out.println("5:42 분입니다");//서여옿
-
 	}
 
 }
