b90957f5a5974793c5a8563dd631d33ce721723c
sooyeun <blind1215@gmail.com>
2020-11-03 17:30:14 +0900

    프린트실험

--- src/project/Test.java
+++ src/project/Test.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -10,6 +12,14 @@ public class Test {
 		// 작성하면 이름 주석으로 달아보면 보기 편할 것 같아요
 		
 		System.out.println("리셋합니다"); // 허준
+		
+		Scanner s = new Scanner(System.in);
+		String test ;
+		
+		System.out.print(" 이것도 쓸수있을까요? ");
+		test = s.nextLine();
+		
+		
 		
 		
 	}
