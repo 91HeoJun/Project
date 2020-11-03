fe40364a98d1155a15834a751f89b98c7671ee7d
[Jun Heo] <[c_atuniel@naver.com]>
2020-11-03 17:53:28 +0900

    의견 1

--- src/project/Test.java
+++ src/project/Test.java
@@ -19,6 +19,9 @@ public class Test {
 		System.out.println("이마트 와인코너 가셔서 모스카토 2병에 18000원에 살수있습니다. 원래는 한병에 15000원 이상이에요"); // 주영우
 		System.out.println("5:42 분입니다");//서여옿
 		System.out.println("연습");//황진구
+		
+		System.out.println("이클립스에서 Git Staging 라인 좌측에 History 부분 보시면 충돌 난 부분이 줄이 겹치는 것 같아요~");
+		System.out.println("정상적으로 올려서 on 으로 들어가면 파란줄에 쭉 올라갈 듯 합니다~");
 	}
 
 }
