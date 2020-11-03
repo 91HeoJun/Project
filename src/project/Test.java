f7a581ab45313cab6f04b9784362522d3df1b60a
[Jun Heo] <[c_atuniel@naver.com]>
2020-11-03 17:19:17 +0900

    테스트1

--- src/project/Test.java
+++ src/project/Test.java
@@ -5,21 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("안녕하세요~");
-		System.out.println("췤 췤 암더안 타카라씨 히파몬 나부라써 뻬이브라써 촤브라써 쿠자스브러 게인자쓰러 비트르비트르 제껴봐라 서브미션 솨피아또빠르 운따르따릉 쫘쪼빠따 빠쏘바야 파스씨야 흐름닦아 부름닦아 프리빼까 킬로빠짜 표파까봐라 계쏙해나바라 계속 계속해봐라 내새까 매끄러부러 케로푸츤 미췬 헤라포스 르 레미키스브러 오닥까락 가락기 기트켠 기타큰혓바닥인더따웃");
+		System.out.println(
+				"췤 췤 암더안 타카라씨 히파몬 나부라써 뻬이브라써 촤브라써 쿠자스브러 게인자쓰러 비트르비트르 제껴봐라 서브미션 솨피아또빠르 운따르따릉 쫘쪼빠따 빠쏘바야 파스씨야 흐름닦아 부름닦아 프리빼까 킬로빠짜 표파까봐라 계쏙해나바라 계속 계속해봐라 내새까 매끄러부러 케로푸츤 미췬 헤라포스 르 레미키스브러 오닥까락 가락기 기트켠 기타큰혓바닥인더따웃");
 		System.out.println("안뇽하세요");
 		System.out.println("hi guys ~  i'm jayce");
 		System.out.println("hi");
-<<<<<<< HEAD
-		
-		// 들어오신분들 입력하실 때 주석으로 이름 달아주시면 알아보기 쉬울 것 같아요~
 
-=======
-
-		System.out.println("수성");
-		
-		System.out.println("이제 오류가 안나요");
-
->>>>>>> branch 'master' of https://github.com/91HeoJun/Project.git
 	}
 
 }
