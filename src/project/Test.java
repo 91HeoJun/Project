2454d4f32b089cc3142d0eff5c41c00c06df7b38
[Jun Heo] <[c_atuniel@naver.com]>
2020-11-03 17:38:57 +0900

    폼 정리

--- src/project/Test.java
+++ src/project/Test.java
@@ -6,17 +6,14 @@ public class Test {
 
 		System.out.println("눈치게임 1");
 
-
 		// 작성하면 이름 주석으로 달아보면 보기 편할 것 같아요
 
 		System.out.println("리셋합니다"); // 허준
 		System.out.println("췍 췍 !"); // 주영우
-		System.out.println("수성"); //서수성
-		
+		System.out.println("수성"); // 서수성
+
 		System.out.println("수성씨, 영우씨 추가 확인했습니다"); // 허준
-		
-		
-		
+
 	}
 
 }
