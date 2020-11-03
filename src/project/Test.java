7cf562b1b04294ff788ed229accb6596f49e28f9
sooyeun <blind1215@gmail.com>
2020-11-03 17:33:01 +0900

    오류 재수정

--- src/project/Test.java
+++ src/project/Test.java
@@ -5,22 +5,14 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("눈치게임 1");
-=======
 
-		System.out.println("눈치게임 1"); //도수연
->>>>>>> branch 'master' of https://github.com/91HeoJun/Project.git
+		System.out.println("눈치게임 1");
+
 
 		// 작성하면 이름 주석으로 달아보면 보기 편할 것 같아요
 		
 		System.out.println("리셋합니다"); // 허준
 		
-		Scanner s = new Scanner(System.in);
-		String test ;
-		
-		System.out.print(" 이것도 쓸수있을까요? ");
-		test = s.nextLine();
 		
 		
 		
